02d791cd49459df4d68e7b30f9c1b830e5b60bea
robloxssop <125984678+robloxssop@users.noreply.github.com>
2025-03-10 16:34:52 +0700

    Update BizBox Project.lua

--- BizBox Project.lua
+++ BizBox Project.lua
@@ -3,7 +3,7 @@ local placeId = game.PlaceId
 if placeId == 71017572336194 then  -- ใส่ PlaceId ของแมพที่ชื่อ B
     loadstring(game:HttpGet("https://raw.githubusercontent.com/robloxssop/Dexexl/refs/heads/main/71017572336194.lua"))()
 elseif placeId == 93140024895832 then  -- ใส่ PlaceId ของแมพที่ชื่อ A
-    loadstring(game:HttpGet(""))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/robloxssop/Dexexl/refs/heads/main/93140024895832.lua"))()
 else
     print("Map not recognized!")
 end
