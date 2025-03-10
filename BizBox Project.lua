c6d05d5268408e2894d966a8721692c105fd873d
robloxssop <125984678+robloxssop@users.noreply.github.com>
2025-03-10 14:56:56 +0700

    Update BizBox Project.lua

--- BizBox Project.lua
+++ BizBox Project.lua
@@ -1,6 +1,6 @@
 local placeId = game.PlaceId
 
-if placeId == 123456789 then  -- ใส่ PlaceId ของแมพที่ชื่อ B
+if placeId == 71017572336194 then  -- ใส่ PlaceId ของแมพที่ชื่อ B
     loadstring(game:HttpGet("https://raw.githubusercontent.com/robloxssop/Dexexl/refs/heads/main/71017572336194.lua"))()
 elseif placeId == 987654321 then  -- ใส่ PlaceId ของแมพที่ชื่อ A
     loadstring(game:HttpGet(""))()
